a26f78a9b2c3ee6104c8337517ce299cc0859df3
Root <Root@DESKTOP-IQHVPPG>
2021-10-10 17:01:07 -0400

    Initial commit

---
+++ HelloWorld-java/src/javadetail/JavaDetailMain.java
@@ -0,0 +1,10 @@
+package javadetail;
+
+public class JavaDetailMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HelloWorld-java/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Root
+ *
+ */
+module HelloWorldJava {
+}
